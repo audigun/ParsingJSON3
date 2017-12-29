63b593b085f98802e27f56b61e6c493ba6e46a89
Audi <audiq@github.com>
2017-12-29 07:54:10 +0700

    Initial Commit

---
+++ ParsingJSON3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParsingJSON3
+//
+//  Created by Audi Dharmawan on 29/12/17.
+//  Copyright © 2017 Audi Dharmawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
